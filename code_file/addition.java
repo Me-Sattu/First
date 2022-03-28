03db62740f1fad062b49ad49f2d2e9524569756f
Satyam Gupta <sattugupta560@gmail.com>
2022-03-28 21:32:28 +0530

    FIrst github file

---
+++ code_file/addition.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class addition{
+ 
+public static void main(String[] args) {
+     Scanner  sc = new Scanner (System.in);
+    //int a = sc.nextInt();
+    //int b = sc.nextInt();
+    //int c = a+b;
+    //int a,b;
+     int a = sc.nextInt(); 
+     int b = sc.nextInt();
+
+    System.out.println("sum = " + (a+b));
+    sc.close();
+}    
+    
+}
